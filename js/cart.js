5df4484858373a89aad886863fa6626c376ff6e8
hellomu <mrs.liu@aliyun.com>
2018-03-06 17:02:09 +0800

    v2.0

--- js/cart.js
+++ js/cart.js
@@ -2,13 +2,18 @@ new Vue({
     el:"#app",
     data:{
         title:"Hello Vue",
-        itemList:[]
+        itemList:[],
+        checkedAllFlag:false,
+        totalPrice:0,
+        delFlag:false
     },
     filters:{
 
     },
     mounted:function(){
-        this.getHttp();
+        this.$nextTick(function(){
+            this.getHttp();
+        })
     },
     methods:{
         getHttp:function(){
@@ -16,6 +21,44 @@ new Vue({
             .then((res)=>{
                 this.itemList=res.data.result.list;
                 console.log(this.itemList)
+            })
+        },
+        computePrice:function(product,way){
+            if(way<0){
+                product.count-=1;
+                if(product.count<1)
+                product.count=1;
+            }else {
+                product.count+=1;
+            }
+            this.computeTotalPrice();
+        },
+        selectedClick:function(product){
+            if(typeof product.checked=="undefined"){
+                //Vue.set(product,"checked",true)
+                this.$set(product,"checked",true)
+            }else {
+                product.checked=!product.checked;
+            }
+            this.computeTotalPrice();
+        },
+        checkedAll:function(flag){
+            this.checkedAllFlag=!this.checkedAllFlag;
+            this.itemList.forEach((value,index)=>{
+                if(typeof value.checked=="undefined"){
+                    this.$set(value,"checked",this.checkedAllFlag)
+                }else {
+                    value.checked=this.checkedAllFlag
+                }
+            })
+            this.computeTotalPrice();
+        },
+        computeTotalPrice(){
+            this.totalPrice=0;
+            this.itemList.forEach((value,idnex)=>{
+                if(value.checked){
+                    this.totalPrice+=value.price*value.count;
+                }
             })
         }
     }
